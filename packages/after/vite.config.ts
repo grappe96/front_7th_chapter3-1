a2bd53152f77d11d614d56dc93583bf848b9651f
grappe96 <w00k0805@naver.com>
2025-11-27 22:59:49 +0900

    feat: GitHub Pages 배포 설정 추가

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/front_7th_chapter3-1/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
